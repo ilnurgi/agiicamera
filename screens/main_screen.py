43c4b6a29cc3ab71f56f30e5a3da9c4ae1fb407a
ilnurgi <ilnurgi@mail.ru>
2022-10-05 12:17:19 +0300

    0.1.3.2

--- screens/main_screen.py
+++ screens/main_screen.py
@@ -4,11 +4,14 @@
 
 __author__ = 'ilnurgi'
 
+import os
 import traceback
+from datetime import datetime
 
 from android.permissions import Permission
 
 from kivy.core.camera import Camera
+from kivy.core.image import Texture
 from kivy.uix.button import Button
 
 from screens.base_screen import BaseScreen
@@ -30,6 +33,7 @@ class MainScreen(BaseScreen):
         self.name = 'main_screen'
         self.title = 'MAIN'
         self.camera = None
+        self.camera_texture: Texture = None
 
     @error
     def _init_content(self):
@@ -43,7 +47,20 @@ class MainScreen(BaseScreen):
         )
         btn_create_camera.bind(on_press=self.create_camera)
 
+        btn_get_photo = Button(
+            text='Сохранить фотку'
+        )
+        btn_get_photo.bind(on_press=self.get_photo)
+
         self.content_container.add_widget(btn_create_camera)
+        self.content_container.add_widget(btn_get_photo)
+
+    @error
+    def on_camera_texture(self, camera):
+        self.camera_texture = camera.texture
+        # self.manager.log_msg('on_tex')
+        # self.manager.log_msg(type(camera.texture))
+        # self.manager.log_msg(camera.texture)
 
     @error
     def create_camera(self, button):
@@ -70,18 +87,19 @@ class MainScreen(BaseScreen):
             return
 
         manager.log_msg('camera created')
-        manager.log_msg(str(dir(self.camera)))
 
-        for f in dir(self.camera):
-            if f.startswith('_'):
-                continue
+        manager.log_msg(type(self.camera.texture))
+        manager.log_to_file(str(self.camera.texture))
 
-            try:
-                a = getattr(self.camera, f)
-                manager.log_to_file(f)
-                manager.log_to_file(str(a))
-                manager.log_to_file(a.__doc__)
-            except Exception as err:
-                manager.log_msg(f)
-                manager.log_msg(str(err))
-                manager.log_msg(traceback.format_exc())
+        self.camera.bind(on_texture=self.on_camera_texture)
+
+    def get_photo(self, button):
+        self.manager.log_msg(self.camera_texture)
+        if not self.camera_texture:
+            return
+
+        app = self.manager.app
+
+        log_file_name = f'{app.app_name}_{datetime.now()}.png'
+        log_file_path = os.path.join(app.public_storage, log_file_name)
+        self.camera_texture.save(log_file_path)
